4ac36444018d666558786968321801503ec86b5c
logesh-77 <80681058+logesh-77@users.noreply.github.com>
2022-01-11 18:27:19 +0530

    Add files via upload

---
+++ UIUX/webAppFrontEnd/src/js/index.js
@@ -0,0 +1,13 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAAmfZD-sijvxfO9_m4KaQjl9HfXs11etI",
+    authDomain: "pqsi-65019.firebaseapp.com",
+    projectId: "pqsi-65019",
+    storageBucket: "pqsi-65019.appspot.com",
+    messagingSenderId: "891498450053",
+    appId: "1:891498450053:web:64c0409ef7054678f5adec",
+    measurementId: "G-X9H5W6R2S6"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
